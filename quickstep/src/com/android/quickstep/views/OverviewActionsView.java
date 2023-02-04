000e8932c5872f11c8e28f9ff405f997bd90d0f5
Trishiraj <gamerprince.exp@gmail.com>
2023-02-04 09:42:22 +0800

    SushiLauncher: Add kill App button to recents overview
    mnri: Add visibility toggle code changes
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- quickstep/src/com/android/quickstep/views/OverviewActionsView.java
+++ quickstep/src/com/android/quickstep/views/OverviewActionsView.java
@@ -98,6 +98,7 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
     private static final String KEY_RECENTS_CLEAR_ALL = "pref_recents_clear_all";
     private static final String KEY_RECENTS_LENS = "pref_recents_lens";
     private static final String KEY_RECENTS_LOCK = "pref_recents_lock";
+    private static final String KEY_RECENTS_KILL_APP = "pref_recents_kill_app";
 
     private MultiValueAlpha mMultiValueAlpha;
     private Button mSplitButton;
@@ -125,6 +126,7 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
     private boolean mClearAll;
     private boolean mLens;
     private boolean mLock;
+    private boolean mKillApp;
 
     public OverviewActionsView(Context context) {
         this(context, null);
@@ -141,6 +143,7 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
         mClearAll = prefs.getBoolean(KEY_RECENTS_CLEAR_ALL, true);
         mLens = prefs.getBoolean(KEY_RECENTS_LENS, false);
         mLock = prefs.getBoolean(KEY_RECENTS_LOCK, true);
+        mKillApp = prefs.getBoolean(KEY_RECENTS_KILL_APP, false);
         prefs.registerOnSharedPreferenceChangeListener(this);
     }
 
@@ -162,6 +165,11 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
         clearall.setOnClickListener(this);
         clearall.setVisibility(mClearAll ? VISIBLE : GONE);
         findViewById(R.id.clear_all_space).setVisibility(mClearAll ? VISIBLE : GONE);
+
+        View killApp = findViewById(R.id.kill_app);
+        killApp.setOnClickListener(this);
+        killApp.setVisibility(mKillApp ? VISIBLE : GONE);
+        findViewById(R.id.kill_app_space).setVisibility(mKillApp ? VISIBLE : GONE);
 
         View lens = findViewById(R.id.action_lens);
         lens.setOnClickListener(this);
@@ -200,6 +208,8 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
             mCallbacks.onClearAllTasksRequested();
         } else if (id == R.id.action_lens) {
             mCallbacks.onLens();
+        } else if (id == R.id.kill_app) {
+            mCallbacks.onKillApp();
         }
     }
 
@@ -226,6 +236,8 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
             mLens = prefs.getBoolean(KEY_RECENTS_LENS, false);
         } else if (key.equals(KEY_RECENTS_LOCK)) {
             mLock = prefs.getBoolean(KEY_RECENTS_LOCK, false);
+        } else if (key.equals(KEY_RECENTS_KILL_APP)) {
+            mKillApp = prefs.getBoolean(KEY_RECENTS_KILL_APP, false);
         }
         updateVisibilities();
     }
