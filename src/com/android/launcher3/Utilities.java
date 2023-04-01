7e26d8478c77ef25a322d43ed13a629db166df9d
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-01 13:41:52 +0800

    Ship as Ortus Launcher
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -947,7 +947,7 @@ public final class Utilities {
     }
 
     public static boolean isActionToastEnabled(Context context) {
-        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_ACTION_TOAST_ENABLED, true);
     }
     
@@ -967,7 +967,7 @@ public final class Utilities {
     }
 
     public static boolean recentsScrollVibration(Context context) {
-    	SharedPreferences prefs = getPrefs(context.getApplicationContext());
+    	SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
     	return prefs.getBoolean(KEY_VIBRATION_TOGGLE, true);
    }
 }
