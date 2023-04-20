f624d790f5f08f8655e962d685a8fddaea0b4725
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-20 12:09:51 +0800

    [WA] Launcher3: Fix quickstep live tile implementation
    * current implementation was drawing the 'live tile' behind the taskview causing live animations behind the focused task (instead of the actual task view)  which is noticeable with scrolling scale animation 8d7f69a and translucent overview
    
    workaround for: live tile not being live (not playing streaming videos on overview) and translucent overview glitches
    
    test: open multiple instance of streaming apps, check if snapshots show live state/media playing on overview state
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Revert "Launcher3: Fix blurry recents issue with live tile"
    
    This reverts commit 0389113a68894f9fc847340be00fabb9f4515cd2.

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -536,6 +536,8 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
     private long mScrollLastHapticTimestamp;
 
     private float mScrollScale = 1f;
+    
+    private boolean mIsLandScape;
 
     /**
      * TODO: Call reloadIdNeeded in onTaskStackChanged.
@@ -1940,6 +1942,8 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
                 || mOrientationState.getRecentsActivityRotation() != ROTATION_0;
         mActionsView.updateHiddenFlags(HIDDEN_NON_ZERO_ROTATION,
                 !mOrientationState.isRecentsActivityRotationAllowed() && isInLandscape);
+                
+	mIsLandScape = isInLandscape;
 
         // Update TaskView's DeviceProfile dependent layout.
         updateChildTaskOrientations();
@@ -1952,6 +1956,10 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
         setCurrentPage(mCurrentPage);
     }
 
+    public boolean getLandScape() {
+        return mIsLandScape;
+    }
+   
     private void onOrientationChanged() {
         // If overview is in modal state when rotate, reset it to overview state without running
         // animation.
@@ -2506,7 +2514,6 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
 
         setEnableFreeScroll(true);
         setEnableDrawingLiveTile(mCurrentGestureEndTarget == GestureState.GestureEndTarget.RECENTS);
-        setRunningTaskViewShowScreenshot(true);
         setRunningTaskHidden(false);
         animateUpTaskIconScale();
         animateActionsViewIn();
@@ -5733,6 +5740,9 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
 
         boolean isInLandscape = mOrientationState.getTouchRotation() != ROTATION_0
                                 && mOrientationState.getTouchRotation() != ROTATION_180;
+
+	mIsLandScape = isInLandscape;
+
         int childCount = Math.min(mPageScrolls.length, getChildCount());
         int curScroll = isInLandscape ? getScrollY() : getScrollX();
 
