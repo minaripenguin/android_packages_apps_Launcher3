447982b4b456c8907d98ed8e0b38ed5adb49de0f
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-23 19:30:23 +0800

    Launcher3: Properly end recents animation views
    * fixes: task views getting stucked ongesture end/swiping recents view
    
    test: manual consecutive app switching/swipe up via recents view
    
    Change-Id: Ib33c468b44d8dd639e5bbccb67c86e6d33fc2389
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -2514,7 +2514,6 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
 
         setEnableFreeScroll(true);
         setEnableDrawingLiveTile(mCurrentGestureEndTarget == GestureState.GestureEndTarget.RECENTS);
-        setRunningTaskViewShowScreenshot(true);
         setRunningTaskHidden(false);
         animateUpTaskIconScale();
         animateActionsViewIn();
